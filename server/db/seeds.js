d2277e57d4502d9d809966dd51a42081216b1163
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-04-19 17:39:05 +0100

    template seed file

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,1 +1,98 @@
 //keep
+const types = [{sea: {}},{air: {}},
+    {"name": "land",
+    "type": [{
+        "name": "car",
+        "sizes": [{
+            "name": "small",
+            "fuel": [{
+                "name": "Diesel",
+                "co2e": 0.14533
+            },{
+                "name": "Petrol",
+                "co2e": 0.15565
+            },{
+                "name": "Hybrid",
+                "co2e": 0.10957
+            },{
+                "name": "Plug-In Hybrid Electric Vehicles",
+                "co2e": 0.02255
+            },{
+                "name": "Electric Vehicle",
+                "co2e": 0.0
+            }]
+        },{
+            "name": "medium",
+            "fuel": [{
+                "name": "Diesel",
+                "co2e": 
+            },{
+                "name": "Petrol",
+                "co2e": 
+            },{
+                "name": "Hybrid",
+                "co2e": 
+            },{
+                "name": "CNG",
+                "co2e": 
+            },{
+                "name": "LPG",
+                "co2e": 
+            },{
+                "name": "Plug-In Hybrid Electric Vehicles",
+                "co2e": 
+            },{
+                "name": "Electric Vehicle",
+                "co2e": 
+            }]
+        },{
+            "name": "large",
+            "fuel": [{
+                "name": "Diesel",
+                "co2e": 
+            },{
+                "name": "Petrol",
+                "co2e": 
+            },{
+                "name": "Hybrid",
+                "co2e": 
+            },{
+                "name": "CNG",
+                "co2e": 
+            },{
+                "name": "LPG",
+                "co2e": 
+            },{
+                "name": "Plug-In Hybrid Electric Vehicles",
+                "co2e": 
+            },{
+                "name": "Electric Vehicle",
+                "co2e": 
+            }]
+        },{
+            "name": "average",
+            "fuel": [{
+                "name": "Diesel",
+                "co2e": 
+            },{
+                "name": "Petrol",
+                "co2e": 
+            },{
+                "name": "Hybrid",
+                "co2e": 
+            },{
+                "name": "CNG",
+                "co2e": 
+            },{
+                "name": "LPG",
+                "co2e": 
+            },{
+                "name": "Plug-In Hybrid Electric Vehicles",
+                "co2e":
+            },{
+                "name": "Electric Vehicle",
+                "co2e": 
+            }]
+        }]
+    },{},{}]}
+]
