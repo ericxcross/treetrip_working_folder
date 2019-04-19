6bb55910b65cda823e0c089fcab2d329d4faff16
ericxcross <eric.cross@sympatico.ca>
2019-04-19 16:07:06 +0100

    file directories added

---
+++ client/src/helpers/request_helper.js
@@ -0,0 +1,40 @@
+const RequestHelper = function (url) {
+ this.url = url;
+};
+
+//INDEX
+RequestHelper.prototype.get = function () {
+ return fetch(this.url)
+   .then((response) => response.json());
+};
+
+//DELETE
+RequestHelper.prototype.delete = function (id) {
+ return fetch(`${this.url}/${id}`, {
+   method: 'DELETE'
+ })
+   .then((response) => response.json());
+};
+
+//CREATE
+RequestHelper.prototype.post = function (payload) {
+ return fetch(`${this.url}/`, {
+   method: 'POST',
+   body: JSON.stringify(payload),
+   headers: { 'Content-Type': 'application/json' }
+ })
+   .then((response) => response.json());
+};
+
+//UPDATE
+RequestHelper.prototype.put = function (id, payload) {
+ return fetch(`${this.url}/${id}`, {
+   method: 'PUT',
+   body: JSON.stringify(payload),
+   headers: { 'Content-Type': 'application/json' }
+ })
+   .then((response) => response.json());
+};
+
+module.exports = RequestHelper;
+module.exports = RequestHelper;
