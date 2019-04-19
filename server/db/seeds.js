cf5149c1cdb633aa1ef48ab0636b504a7ec1f92e
Alasdair Carstairs <alasdaircarstairs@Agnes-MacBook-Pro.local>
2019-04-19 17:51:23 +0100

    changes to seed file

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -25,49 +25,49 @@ const types = [{sea: {}},{air: {}},
             "name": "medium",
             "fuel": [{
                 "name": "Diesel",
-                "co2e": 
+                "co2e": 0.17353
             },{
                 "name": "Petrol",
-                "co2e": 
+                "co2e": 0.19386
             },{
                 "name": "Hybrid",
-                "co2e": 
+                "co2e": 0.11538
             },{
                 "name": "CNG",
-                "co2e": 
+                "co2e": 0.16324
             },{
                 "name": "LPG",
-                "co2e": 
+                "co2e": 0.18217
             },{
                 "name": "Plug-In Hybrid Electric Vehicles",
-                "co2e": 
+                "co2e": 0.0712
             },{
                 "name": "Electric Vehicle",
-                "co2e": 
+                "co2e": 0.0
             }]
         },{
             "name": "large",
             "fuel": [{
                 "name": "Diesel",
-                "co2e": 
+                "co2e": 0.2152
             },{
                 "name": "Petrol",
-                "co2e": 
+                "co2e": 0.28411
             },{
                 "name": "Hybrid",
-                "co2e": 
+                "co2e": 0.16134
             },{
                 "name": "CNG",
-                "co2e": 
+                "co2e": 0.23851
             },{
                 "name": "LPG",
-                "co2e": 
+                "co2e": 0.26704
             },{
                 "name": "Plug-In Hybrid Electric Vehicles",
-                "co2e": 
+                "co2e": 0.07717
             },{
                 "name": "Electric Vehicle",
-                "co2e": 
+                "co2e": 0.0
             }]
         },{
             "name": "average",
