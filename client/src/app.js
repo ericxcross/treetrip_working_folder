3bb1c3c670bffbcae2dc367689966770f9b8d722
ericxcross <eric.cross@sympatico.ca>
2019-04-20 16:30:40 +0100

    app.js for MVP completed

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,19 @@
-// KEEP
+const FormView = require('./views/form_view.js')
+const ResultTotalView = require('./views/result_total_view.js');
+const CarbonCounter = require('./models/carbon_counter.js'); //CHECK IF CORRECT
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const formElement = document.querySelector('form#carbon-form');
+  const formView = new FormView(formElement);
+  formView.bindEvents();
+
+  const resultTotalElement = document.querySelector('div#result-total');
+  const resultTotalView = new ResultTotalView(resultTotalElement);
+  resultTotalView.bindEvents();
+
+  const carbonCounter = new CarbonCounter();
+  carbonCounter.bindEvents();
+  carbonCounter.getData();
+
+});
