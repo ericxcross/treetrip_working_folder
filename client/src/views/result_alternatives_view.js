a3da03b8a9bbdeb3dcb83f61e897907fe6f178d3
Myriam Boran <brnmyriam45@googlemail.com>
2019-04-23 15:06:35 +0100

    added graph

--- client/src/views/result_alternatives_view.js
+++ client/src/views/result_alternatives_view.js
@@ -1,53 +1,45 @@
 const PubSub = require("../helpers/pub_sub.js");
 const RequestHelper = require("../helpers/request_helper.js");
 
-const ResultAlternativeView = function (container) {
-    this.container = container;
+const ResultAlternativeView = function(container) {
+  this.container = container;
 };
 
-ResultAlternativeView.prototype.bindEvents = function () {
-
-    PubSub.subscribe("CarbonCounter:AlternativeTravelOptions", (evt) => {
-        this.container.innerHTML = "";
-        // separate incoming data
-        const alternativesData = evt.detail;
-        let transportTypes = [];
-        let treesChange = [];
-        alternativesData.forEach((type) => {
-            transportTypes.push(type.name);
-            treesChange.push(type.treesChange);
-        });
-        const seriesData = [{
-            name: 'Trees',
-            data: treesChange
-        }]
-        var myChart = Highcharts.chart('alternatives', {
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: 'Transport Alternatives'
-            },
-            xAxis: {
-                categories: transportTypes
-            },
-            yAxis: {
-                title: {
-                    text: 'Difference in Trees'
-                }
-            },
-            series: seriesData,
-
-
-            zones: [{
-                value: -10,
-                color: `#CE3815`
-            }, {
-                value: 0,
-                color: `#99D47F`
-            }]
-
-        });
+ResultAlternativeView.prototype.bindEvents = function() {
+  PubSub.subscribe("CarbonCounter:AlternativeTravelOptions", evt => {
+    this.container.innerHTML = "";
+    // separate incoming data
+    const alternativesData = evt.detail;
+    let transportTypes = [];
+    let treesChange = [];
+    alternativesData.forEach(type => {
+      transportTypes.push(type.name);
+      treesChange.push(type.treesChange);
     });
+    const seriesData = [
+      {
+        name: "Trees",
+        data: treesChange
+      }
+    ];
+    var myChart = Highcharts.chart("alternatives", {
+      chart: {
+        type: "bar"
+      },
+      title: {
+        text: "Transport Alternatives"
+      },
+      xAxis: {
+        categories: transportTypes
+      },
+      yAxis: {
+        title: {
+          text: "Difference in Trees"
+        }
+      },
+      series: seriesData
+    });
+  });
 };
+
 module.exports = ResultAlternativeView;
