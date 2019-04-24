31483b82d2dd4ad0950b3db2924d16940585ca69
Myriam Boran <brnmyriam45@googlemail.com>
2019-04-24 11:43:09 +0100

    updated content seeds

--- client/src/views/result_total_view.js
+++ client/src/views/result_total_view.js
@@ -1,4 +1,5 @@
 const PubSub = require("../helpers/pub_sub.js");
+const content_text = require("../content/result_view_content.js");
 
 const ResultTotalView = function(element) {
   this.element = element;
@@ -7,31 +8,28 @@ const ResultTotalView = function(element) {
 ResultTotalView.prototype.bindEvents = function() {
   PubSub.subscribe("CarbonCounter:OutputData", evt => {
     this.element.innerHTML = "";
-    this.renderHexagon(
-      "co2-result",
-      `${evt.detail.co2e}kg`,
-      `of CO<sub>2</sub>e`
-    );
+    this.renderHexagon("co2-result", `${evt.detail.co2e}kg`, content_text.co2e);
     this.renderHexagon(
       "tree-result",
       `${evt.detail.trees} trees`,
-      "to offset emissions"
+      content_text.trees
     );
-    this.renderHexagon("sc-result", `£ ${evt.detail.sc}`, "social cost");
+    this.renderHexagon("sc-result", `£ ${evt.detail.sc}`, content_text.sc);
   });
 };
 
-ResultTotalView.prototype.renderHexagon = function(divId, text, subtext) {
+ResultTotalView.prototype.renderHexagon = function(divId, text, object) {
   const containerDiv = document.createElement("div");
-  containerDiv.id = divId;
-  containerDiv.classList.add(`hexagon`);
-  const header = document.createElement("h2");
-  header.innerHTML = text;
-  const paragraph = document.createElement("p");
-  paragraph.innerHTML = subtext;
 
-  containerDiv.appendChild(header);
-  containerDiv.appendChild(paragraph);
+  containerDiv.innerHTML = `
+    <div class="info-card">
+      <image src=${object.url} alt="background image">
+      <h2>${text}</h2>
+      <p>${object.subtext}</p>
+    </div>
+    <p>${object.paragraph}</p>
+  `;
+
   this.element.appendChild(containerDiv);
 };
 
