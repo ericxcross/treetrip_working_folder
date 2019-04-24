df171c6ee41191deaccd0418f68b322e4b6c49d6
Myriam Boran <brnmyriam45@googlemail.com>
2019-04-24 13:38:42 +0100

    resulttotalview oriented in flexbox

--- client/src/views/result_total_view.js
+++ client/src/views/result_total_view.js
@@ -11,7 +11,7 @@ ResultTotalView.prototype.bindEvents = function() {
     this.renderHexagon("co2-result", `${evt.detail.co2e}kg`, content_text.co2e);
     this.renderHexagon(
       "tree-result",
-      `${evt.detail.trees} trees`,
+      `${evt.detail.trees}`,
       content_text.trees
     );
     this.renderHexagon("sc-result", `£ ${evt.detail.sc}`, content_text.sc);
@@ -20,13 +20,14 @@ ResultTotalView.prototype.bindEvents = function() {
 
 ResultTotalView.prototype.renderHexagon = function(divId, text, object) {
   const containerDiv = document.createElement("div");
+  containerDiv.id = divId;
+  containerDiv.classList.add("hexagon");
 
   containerDiv.innerHTML = `
-    <div class="info-card">
-      <image src=${object.url} alt="background image">
+    <div class="info-card" style="background-image: url(${object.url})">
       <h2>${text}</h2>
-      <p>${object.subtext}</p>
     </div>
+    <h3>${object.subtext}</h3>
     <p>${object.paragraph}</p>
   `;
 
